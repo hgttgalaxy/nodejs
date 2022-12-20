7ccf0baa05a52e1ff1a8558ecb7abe8d97da9f2d
hgttgalaxy <hgttgalaxy@gmail.com>
2022-12-20 22:05:16 +0900

    1. 폼수정 2. app.use로 npm 라이브러리로 추가

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ app.listen(8080, function(){
 
 //누군가가 /pet 으로 방문을 하면.. 
 //pet 관련된 안내문을 띄어주자
-
-app.get('/pet', function(req, res){
-    res.send('냐오오옹');
-});
+app.use(express.urlencoded({extended: true}))
 
 app.get('/beauty', function(req, res){
     res.send('립스틱');
@@ -18,4 +15,8 @@ app.get('/beauty', function(req, res){
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html')
 });
