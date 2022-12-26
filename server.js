38a74933685df2a679dba9ad91926da5e3c633d9
hgttgalaxy <hgttgalaxy@gmail.com>
2022-12-26 09:43:25 +0900

    1226_office

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ app.listen(8080, function(){
     console.log('listening on 8080')
 });
 
-//누군가가 /pet 으로 방문을 하면.. 
-//pet 관련된 안내문을 띄어주자
+
 app.use(express.urlencoded({extended: true}))
 
-app.get('/beauty', function(req, res){
-    res.send('립스틱');
-});
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
